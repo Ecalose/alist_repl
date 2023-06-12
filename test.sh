5660fc2092104e4dd1e6dbf6b785aec0a0be1bfe
k0baya <132335755+k0baya@users.noreply.github.com>
2023-06-13 00:25:12 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -13,7 +13,24 @@ mkdir -p program && mkdir -p tempupdate
 tar -xzf manager.tar.gz -C tempupdate && rm -f manager.tar.gz && mv -f tempupdate/* program/manager && rm -rf tempupdate && cd program && chmod +x manager && cd /home/runner/${REPL_SLUG}
 
 if [ -s "main.sh" ]; then
-    echo "nohup ./background/program/manager admin > /home/runner/\${REPL_SLUG}/templog.txt && sed -n -e 's/username: \(.*\)/username: \1/p' -e 's/password: \(.*\)/password: \1/p' /home/runner/\${REPL_SLUG}/templog.txt > user_info.txt && rm /home/runner/\${REPL_SLUG}/templog.txt && nohup ./background/program/manager server >/dev/null 2>&1" > main.sh
+    echo "nohup ./background/program/manager admin > /home/runner/\${REPL_SLUG}/templog.txt
+sed -n -e 's/username: \(.*\)/username: \1/p' -e 's/password: \(.*\)/password: \1/p' /home/runner/\${REPL_SLUG}/templog.txt > user_info.txt
+rm /home/runner/\${REPL_SLUG}/templog.txt
+user_info_content=\$(cat user_info.txt)
+echo \"Your account:\"
+echo \"\${user_info_content}\"
+echo \"Just enjoy    :)\"
+rm -f user_info.txt
+nohup ./background/program/manager server >/dev/null 2>&1" > main.sh
 else
-    echo "nohup ./background/program/manager admin > /home/runner/\${REPL_SLUG}/templog.txt && sed -n -e 's/username: \(.*\)/username: \1/p' -e 's/password: \(.*\)/password: \1/p' /home/runner/\${REPL_SLUG}/templog.txt > user_info.txt && rm /home/runner/\${REPL_SLUG}/templog.txt && nohup ./background/program/manager server >/dev/null 2>&1" > main.sh
+    echo "nohup ./background/program/manager admin > /home/runner/\${REPL_SLUG}/templog.txt
+sed -n -e 's/username: \(.*\)/username: \1/p' -e 's/password: \(.*\)/password: \1/p' /home/runner/\${REPL_SLUG}/templog.txt > user_info.txt
+rm /home/runner/\${REPL_SLUG}/templog.txt
+user_info_content=\$(cat user_info.txt)
+echo \"Your account:\"
+echo \"\${user_info_content}\"
+echo \"Just enjoy    :)\"
+rm -f user_info.txt
+nohup ./background/program/manager server >/dev/null 2>&1" > main.sh
 fi
+
