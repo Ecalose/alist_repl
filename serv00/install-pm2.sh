20f3057c528052f05ed57cb3aa6986ebc5d8f77e
k0baya <132335755+k0baya@users.noreply.github.com>
2024-01-27 18:03:33 +0800

    Create install-pm2.sh

---
+++ serv00/install-pm2.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/.npm-global && npm config set prefix '~/npm-global' && echo 'export PATH=~/npm-global/bin:$PATH' >> ~/.bashrc && source ~/.bashrc && npm install -g pm2
+echo "如果下次重新登陆SSH后pm2的工作目录变量设置失效，你可以重新执行本脚本或者直接使用 ~/npm-global/bin/pm2 来调用pm2"
