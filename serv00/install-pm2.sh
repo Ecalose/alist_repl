3f2cc0c0f60812cc95a86575e33ead65ae759271
k0baya <132335755+k0baya@users.noreply.github.com>
2024-04-04 17:13:11 +0800

    Update install-pm2.sh

--- serv00/install-pm2.sh
+++ serv00/install-pm2.sh
@@ -1,2 +1,2 @@
-mkdir -p ~/.npm-global && npm config set prefix '~/.npm-global' && echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.bashrc && source ~/.bashrc && npm install -g pm2
-echo "请使用 ~/.npm-global/bin/pm2 来调用pm2"
+mkdir -p ~/.npm-global && npm config set prefix '~/.npm-global' && echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile && source ~/.profile && npm install -g pm2
+echo "pm2 成功安装，请不要删除或覆盖 ~/.profile 配置文件"
