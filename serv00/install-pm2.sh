2544ec13df472dfe88bb3bbac0ffd71df9703e08
k0baya <132335755+k0baya@users.noreply.github.com>
2024-04-04 17:43:27 +0800

    Update install-pm2.sh

--- serv00/install-pm2.sh
+++ serv00/install-pm2.sh
@@ -1,2 +1,19 @@
 mkdir -p ~/.npm-global && npm config set prefix '~/.npm-global' && echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile && source ~/.profile && npm install -g pm2
+clear
+echo
+echo
+echo
+echo
+echo
 echo "pm2 成功安装，请不要删除或覆盖 ~/.profile 配置文件"
+echo
+echo
+echo
+echo "更多应用部署请参照 https://blog.rappit.site/ "
+echo
+echo
+ecoh
+echo "欢迎加入 QQ 群一同学习探讨：738386033 "
+echo
+echo
+echo
