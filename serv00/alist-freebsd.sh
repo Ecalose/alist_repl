67bdb26cbfb715af4ca450eb778e977e96f386a7
k0baya <132335755+k0baya@users.noreply.github.com>
2024-07-03 15:32:53 +0800

    Update alist-freebsd.sh

--- serv00/alist-freebsd.sh
+++ serv00/alist-freebsd.sh
@@ -13,16 +13,6 @@ if [ -f "./data/config.json" ]; then
 else
     nohup ./alist server > /dev/null 2>&1 &
     clear
-    echo "已生成配置文件，请修改端口！"
-    echo
-    echo "使用命令 cd data 进入data路径下"
-    echo
-    echo "再使用 vim config.json 命令，对配置文件进行编辑，按 i 进入插入模式"
-    echo
-    echo "将 port: 5244中的 5244 修改成你放行的端口即可"
-    echo
-    echo "接着按 Esc 键退出插入模式，再按 : 键进入命令模式，并输入 wq 再回车，保存并退出"
-    echo
-    echo "再使用命令 cd .. 回到上级目录"
+    echo "已生成配置文件，请在 Panel 中修改配置文件！"
     echo
 fi
